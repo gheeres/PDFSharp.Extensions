3aea53457c12b022d32e4138714583bcb348fb55
George H <gheeres@uwsp.edu>
2014-03-27 10:48:48 -0500

    Add extension method for System.Drawing.Image to convert an Image to a PdfPage or to convert a IEnumerable<Image> to a PdfDocument.

---
+++ System/Drawing/ImageExtensions.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+
+// ReSharper disable once CheckNamespace
+namespace System.Drawing
+{
+  /// <summary>
+  /// Extension methods for the <see cref="Image"/> class.
+  /// </summary>
+  public static class ImageExtensions
+  {
+    /// <summary>
+    /// Generates a PDF page from the image.
+    /// </summary>
+    /// <param name="image">The image to convert to a <see cref="PdfPage"/>.</param>
+    /// <returns>The <see cref="PdfPage"/> containing the image.</returns>
+    public static PdfPage ToPdfPage(this Image image)
+    {
+      if (image == null) return(new PdfPage());
+    
+      PdfPage page = new PdfPage() {
+        Width = image.Width,
+        Height = image.Height
+      };
+      XGraphics xGraphics = XGraphics.FromPdfPage(page);
+      XImage xImage = XImage.FromGdiPlusImage(image);
+      xGraphics.DrawImage(xImage, 0, 0, image.Width, image.Height);
+
+      return (page);
+    }
+
+    /// <summary>
+    /// Generates a PDF page from the image.
+    /// </summary>
+    /// <param name="image">The image to convert to a <see cref="PdfPage"/>.</param>
+    /// <returns>The <see cref="PdfPage"/> containing the image.</returns>
+    public static PdfDocument ToPdf(this Image image)
+    {
+      if (image == null) return (new PdfDocument());
+      return (ToPdf(new[] { image }));
+    }
+
+    /// <summary>
+    /// Generates a PDF document from the collection of enumerations with each image representing
+    /// a new page.
+    /// </summary>
+    /// <param name="images">The collection of images to convert to pages.</param>
+    /// <returns>The <see cref="PdfDocument"/> containing the images.</returns>
+    public static PdfDocument ToPdf(this IEnumerable<Image> images)
+    {
+      PdfDocument document = new PdfDocument();
+      foreach (var image in images) {
+        PdfPage page = ToPdfPage(image);
+        if (page != null) {
+          document.AddPage(page);
+        }
+      }
+      return (document);
+    }
+  }
+}
